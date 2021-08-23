9d2a5546f77b7423431588a89809a6e53becf135
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-08-23 12:02:35 -0400

    adds first 4 functions

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 /**
  * Appends an element or multiple elements to the end of an array. Do not use the push method.
  * @param {Array[]} array - an array of with any kind of elements.
+ * @param (*) element - an element of any kind.
  * @returns {number} the new length of the array.
  */
 
-function myPushFunction() {}
+function myPushFunction(array, element) {
+  array[array.length] = element
+  return array.length
+}
 
 /**
  * Removes the last element of the array and returns the removed element or undefined if the array is empty. Do not use the pop method.
  * @param {Array[]} array - an array of with any kind of elements.
+ * @param (*) element - an element of any kind.
  * @returns {number} the new length of the array .
  */
-function myPopFunction() {}
+function myPopFunction(array, element) {
+  if(!array.length) {
+    return undefined
+  }
+  let last = array[array.length - 1]
+  array.length = array.length - 1
+  return last
+  
+}
 
 /**
  * Determine whether an array includes a specific value within its entries. Do not use the includes method.
@@ -19,7 +32,16 @@ function myPopFunction() {}
  * @param {*} searchElement - an element of any type that you are searching for in the array.
  * @returns {boolean} returns true or false if the searchElement is found in the array.
  */
-function myIncludesFunction() {}
+function myIncludesFunction(array, searchElement) {
+let count = array.length;
+  for(let i = 0; i < count; i++) {
+    if (array[i] === searchElement) {
+      return true
+    }
+  }
+    return false
+  }
+
 
 /**
  * Return the first index in an array where a specified value is found. Do not use the indexOf method.
@@ -27,7 +49,14 @@ function myIncludesFunction() {}
  * @param {*} searchElement - an element of any type that you are searching for in the array.
  * @returns {number} returns the index at with the searchElement is found or -1.
  */
-function myIndexOfFunction() {}
+function myIndexOfFunction(array, searchElement) {
+for (let i = 0; i < array.length; i++){
+  if(array[i] === searchElement){
+     return i
+   }
+}
+return - 1;
+}
 
 /**
  * Returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified. Do not use the slice method.
@@ -36,7 +65,11 @@ function myIndexOfFunction() {}
  * @param {number} [endIdx] - an optional number representing the ending index of the extraction (non-inclusive). The endIdx can be negative.
  * @returns {Array[*]} returns a new array containing the extracted elements.
  */
-function mySliceFunction() {}
+function mySliceFunction(array, startIdx, endIdx) {
+ for (let i = startIdx; i < array[endIdx]; i++) {
+   
+ }
+}
 
 /**
  * Creates and returns a new string by concatenating all of the elements in an array, separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator. Do not use the join method.
